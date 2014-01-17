8f9be3820091051a1e533648c6dae8f3068a4bd0
yulyat <truskinovsky@gmail.com>
2014-01-17 10:24:05 -0500

    plot ages

--- sampleR.R
+++ sampleR.R
@@ -1,2 +1,22 @@
 #holla
 #bloggety blog
+
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(stargazer)
+
+#------------
+# Read data
+#------------
+medals <- read.csv("http://andhs.co/olympics")
+
+#----------------
+# Medal winners
+#----------------
+# Age of medal winners
+fig1 <- ggplot(medals, aes(x=Age))
+fig1 <- fig1 + geom_bar(stat="bin", binwidth=1) + 
+  labs(x="Age", y="Count", title="Age of Olympic medal winners, 2000-2012\n")
+fggsave(plot=fig1, filename="figure_1.pdf", width=7, height=5, units="in")
+
